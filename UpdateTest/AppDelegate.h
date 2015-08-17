8457b96c0a55a6aa4f63cf017a19bae64e0df1ab
Cloudox <cloudox@foxmail.com>
2015-08-17 09:39:38 +0800

    Initial Commit

---
+++ UpdateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UpdateTest
+//
+//  Created by csdc-iMac on 15/8/17.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
